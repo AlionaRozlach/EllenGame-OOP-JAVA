60ba9364864533604c3e9f68dd7a4acdbcfed68b
Alona Rozlach <alona.rozlach@student.tuke.sk>
2019-11-01 21:48:40 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sk/trt.java

--- src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
+++ src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
@@ -1,24 +1,27 @@
 package sk.tuke.kpi.oop.game;
 
+import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
-import sk.tuke.kpi.gamelib.Scene;
+
 
 public class Helicopter extends AbstractActor {
 
     private Animation helicopter;
-   // private Player player;
+   private Player player;
 
     //private Actor helik;
     //private Actor play;
 
     public Helicopter() {
-        helicopter = new Animation("sprites/heli.png",64,64,0.07f);
+        helicopter = new Animation("sprites/heli.png",64,64,0.07f, Animation.PlayMode.LOOP_REVERSED);
         setAnimation(helicopter);
+        new Loop<>(new Invoke<>(this::searchAndDestroy)).scheduleFor(this);
     }
 
- /*  public void searchAndDestroy()
+  public void searchAndDestroy()
     {
         player = (Player)getScene().getFirstActorByName("Player");
         int x_act = player.getPosX();
@@ -27,7 +30,23 @@ public class Helicopter extends AbstractActor {
         int x_helic = getPosX();
         int y_helic = getPosY();
 
-        while ()
+        int x;
+        int y;
 
-    }*/
+        if(y_act>y_helic) y = 1+y_helic;
+        else y = y_helic -1;
+
+        if(x_act>x_helic) x = 1+x_helic;
+        else x = x_helic - 1;
+
+        setPosition(x,y);
+
+        int energ = player.getEnergy();
+        boolean kontakt = intersects(player);
+
+        if(kontakt == true){
+            player.setEnergy(energ-1);
+        }
+
+    }
 }
