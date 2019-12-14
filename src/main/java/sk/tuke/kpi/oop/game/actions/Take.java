170d477602340711272a49e4659a13e8dc5a3855
Alona Rozlach <alona.rozlach@student.tuke.sk>
2019-12-14 12:49:43 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sk/trt.java

--- src/main/java/sk/tuke/kpi/oop/game/actions/Take.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Take.java
@@ -2,6 +2,7 @@ package sk.tuke.kpi.oop.game.actions;
 
 
 import sk.tuke.kpi.gamelib.Actor;
+import sk.tuke.kpi.gamelib.GameApplication;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
 import sk.tuke.kpi.oop.game.Keeper;
@@ -16,19 +17,29 @@ public class Take <A extends Keeper> extends AbstractAction<A> {
     @Override
     public void execute(float deltaTime) {
         Actor actor=getActor();
-        Scene scene = actor.getScene();
-        Backpack backpack = getActor().getBackpack();
+        if(actor!=null) {
+            Scene scene = actor.getScene();
+            Backpack backpack = getActor().getBackpack();
 
 
-        if(actor!= null && scene != null && backpack!= null && backpack.getCapacity()!=backpack.getSize()) {
-            List<Actor> myList = scene.getActors();
+            if ( scene != null && backpack != null && backpack.getCapacity() != backpack.getSize()) {
+                List<Actor> myList = scene.getActors();
 
-           for (int i = 0; i != myList.size(); i++) {
-                if (myList.get(i) instanceof Collectible) {
-                    if (myList.get(i).intersects(getActor())) {
-
-                        backpack.add((Collectible) myList.get(i));
-                        scene.removeActor(myList.get(i));
+                for (int i = 0; i != myList.size(); i++) {
+                    if (myList.get(i) instanceof Collectible) {
+                        if (myList.get(i).intersects(getActor())) {
+                           /* try {
+                                backpack.add((Collectible) myList.get(i));
+                            } catch (Exception e) {
+                                int windowHeight = getActor().getScene().getGame().getWindowSetup().getHeight();
+                                int yTextPos = windowHeight - GameApplication.STATUS_LINE_OFFSET;
+                                getActor().getScene().getGame().getOverlay().drawText(e.getMessage(), 0, yTextPos - 30).showFor(2);
+                                return;
+                            }*/
+                           vynimka(backpack,myList,i);
+                            scene.removeActor(myList.get(i));
+                            break;
+                        }
                     }
                 }
             }
@@ -38,4 +49,15 @@ public class Take <A extends Keeper> extends AbstractAction<A> {
         //vynimka
     }
 
+    public void vynimka(Backpack backpack,List<Actor> myList,int i)
+    {
+        try {
+            backpack.add((Collectible) myList.get(i));
+        } catch (Exception e) {
+            int windowHeight = getActor().getScene().getGame().getWindowSetup().getHeight();
+            int yTextPos = windowHeight - GameApplication.STATUS_LINE_OFFSET;
+            getActor().getScene().getGame().getOverlay().drawText(e.getMessage(), 0, yTextPos - 30).showFor(2);
+            return;
+        }
+    }
 }
