5f0b8d7fc439cab26b362ee2700c880c796ef7c5
Alona Rozlach <alona.rozlach@student.tuke.sk>
2019-12-15 22:51:07 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sk/trt.java

--- src/main/java/sk/tuke/kpi/oop/game/behaviours/Observing.java
+++ src/main/java/sk/tuke/kpi/oop/game/behaviours/Observing.java
@@ -1,4 +1,37 @@
 package sk.tuke.kpi.oop.game.behaviours;
 
-public class Observing {
+import sk.tuke.kpi.gamelib.Actor;
+import sk.tuke.kpi.gamelib.messages.Topic;
+
+import java.util.function.Predicate;
+
+public class Observing<A extends Actor, T> implements Behaviour<A> {
+
+    private Predicate<T> predicate;
+    private Behaviour<A> delegate;
+    private Topic<T> topic;
+
+
+
+    public Observing( Predicate<T> predicate,Behaviour<A> delegate,Topic<T> topic) {
+        this.topic = topic;
+        this.predicate = predicate;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void setUp(A actor) {
+        if (actor == null) {
+            return;
+        }
+        cyklusDekorate(actor);
+    }
+    private void cyklusDekorate(A actor)
+    {
+        actor.getScene().getMessageBus().subscribe(this.topic, k -> {
+            if (predicate.test(k)){
+                delegate.setUp(actor);
+            }
+        });
+    }
 }
