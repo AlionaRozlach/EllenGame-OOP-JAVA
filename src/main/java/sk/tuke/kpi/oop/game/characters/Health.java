8d8fbd01b52415982e5d73495c66cc1a6dc196aa
Alona Rozlach <alona.rozlach@student.tuke.sk>
2019-12-15 09:52:26 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sk/trt.java

--- src/main/java/sk/tuke/kpi/oop/game/characters/Health.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/Health.java
@@ -1,19 +1,42 @@
 package sk.tuke.kpi.oop.game.characters;
 
+import java.util.ArrayList;
+
 public class Health {
+
+    @FunctionalInterface
+    public interface ExhaustionEffect {
+        void apply();
+    }
+
 
     private int zac_health;
     private int max_health;
+    private ArrayList<ExhaustionEffect> observer;
+
+
 
     public Health(int zaciatok, int max) {
         zac_health = zaciatok;
         this.max_health = max;
+        observer=new ArrayList<>();
     }
 
     public Health(int max)
     {
         zac_health = max;
         this.max_health = max;
+        observer=new ArrayList<>();
+    }
+
+    public void sendToObservers(){
+        for (int i=0;i<observer.size();i++){
+            observer.get(i).apply();
+        }
+    }
+
+    public void onExhaustion(ExhaustionEffect effect){
+        this.observer.add(effect);
     }
 
     public int getValue()
@@ -38,19 +61,25 @@ public class Health {
     public void drain(int amount)
     {
         int modul = Math.abs(amount);
-        if(zac_health<=0) return;
+        if(zac_health<0) return;
 
         zac_health-=modul;
         if(zac_health<0)
         {
             zac_health=0;
         }
+        if (zac_health==0)
+        {
+            this.sendToObservers();
+        }
+
+
     }
 
     public void exhaust()
     {
         if(zac_health == 0){
-            zac_health=0;
+            this.sendToObservers();
         }
         zac_health = 0;
     }
--- src/main/java/sk/tuke/kpi/oop/game/scenarios/EscapeRoom.java
+++ src/main/java/sk/tuke/kpi/oop/game/scenarios/EscapeRoom.java
@@ -4,6 +4,9 @@ import org.jetbrains.annotations.Nullable;
 import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.ActorFactory;
 import sk.tuke.kpi.gamelib.SceneListener;
+import sk.tuke.kpi.oop.game.characters.Alien;
+import sk.tuke.kpi.oop.game.items.Ammo;
+import sk.tuke.kpi.oop.game.items.Energy;
 
 public class EscapeRoom implements SceneListener {
 
@@ -13,7 +16,14 @@ public class EscapeRoom implements SceneListener {
 
         @Override
         public @Nullable Actor create(@Nullable String type, @Nullable String name) {
-            return null;
+            if(name!=null)
+            {
+                if(name=="alien") return new Alien();
+                else if(name=="energy") return new Energy();
+                else if(name == "ammo") return new Ammo();
+                else return null;
+            }
+            else return null;
         }
     }
 }
