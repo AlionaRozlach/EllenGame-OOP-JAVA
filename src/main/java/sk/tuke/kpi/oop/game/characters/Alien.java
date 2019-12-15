be30f865fc55d712007b998bdf2f5bd6fa31d7c8
Alona Rozlach <alona.rozlach@student.tuke.sk>
2019-12-15 08:56:12 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sk/trt.java

--- src/main/java/sk/tuke/kpi/oop/game/characters/Alien.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/Alien.java
@@ -1,20 +1,31 @@
 package sk.tuke.kpi.oop.game.characters;
 
+
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.Movable;
 
-public class Alien extends AbstractActor implements Movable {
+public class Alien extends AbstractActor implements Movable,Enemy,Alive {
+
+    private Health health;
 
     public Alien()
     {
         Animation ally;
         ally = new Animation("sprites/alien.png",32,32,0.1f);
         setAnimation(ally);
+        health=new Health(100);
     }
 
     @Override
     public int getSpeed() {
         return 2;
     }
+
+    @Override
+    public Health getHealth() {
+        return health;
+    }
+
+
 }
--- src/main/java/sk/tuke/kpi/oop/game/characters/Health.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/Health.java
@@ -54,4 +54,6 @@ public class Health {
         }
         zac_health = 0;
     }
+
+
 }
